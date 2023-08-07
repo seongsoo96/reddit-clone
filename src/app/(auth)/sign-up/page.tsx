22e7ca2ec8912750d4c61cfaa879af0754a57113
seongsoo96 <seongsoo.dev@gmail.com>
2023-08-07 15:37:46 +0900

    feat: authModal intercepting 추가

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import { buttonVariants } from '@/components/ui/Button';
 import { cn } from '@/lib/utils';
-import SignIn from '@/components/SignIn';
+import SignUp from '@/components/SignUp';
 import { ChevronLeft } from 'lucide-react';
 
 const page = () => {
@@ -20,7 +20,7 @@ const page = () => {
           Home
         </Link>
 
-        <SignIn />
+        <SignUp />
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,10 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
     <html
@@ -25,7 +27,10 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen bg-slate-50 pt-12 antialiased">
+        {/* @ts-expect-error server component */}
         <Navbar />
+
+        {authModal}
 
         <div className="container mx-auto h-full max-w-7xl pt-12">
           {children}
