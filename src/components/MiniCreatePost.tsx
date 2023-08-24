76d78466d56a2b076d3098649c82a167ece34b1b
seongsoo96 <seongsoo.dev@gmail.com>
2023-08-24 17:46:16 +0900

    feat: css 수정

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -17,7 +17,7 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
   const pathname = usePathname();
 
   return (
-    <li className="overflow-hidden rounded-md bg-white shadow">
+    <li className="overflow-hidden rounded-md bg-white shadow block">
       <div className="h-full px-6 py-4 flex justify-between gap-6">
         <div className="relative">
           <UserAvatar
@@ -28,27 +28,26 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
           />
 
           <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
-
-          <Input
-            readOnly
-            onClick={() => router.push(pathname + '/submit')}
-            placeholder="Create post"
-          />
-
-          <Button
-            variant="ghost"
-            onClick={() => router.push(pathname + '/submit')}
-          >
-            <ImageIcon className="text-zinc-600" />
-          </Button>
-
-          <Button
-            variant="ghost"
-            onClick={() => router.push(pathname + '/submit')}
-          >
-            <Link2 className="text-zinc-600" />
-          </Button>
         </div>
+        <Input
+          readOnly
+          onClick={() => router.push(pathname + '/submit')}
+          placeholder="Create post"
+        />
+
+        <Button
+          variant="ghost"
+          onClick={() => router.push(pathname + '/submit')}
+        >
+          <ImageIcon className="text-zinc-600" />
+        </Button>
+
+        <Button
+          variant="ghost"
+          onClick={() => router.push(pathname + '/submit')}
+        >
+          <Link2 className="text-zinc-600" />
+        </Button>
       </div>
     </li>
   );
